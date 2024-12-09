16521b46ae309c15b75c74c9fc2e9dba71efcf01
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2024-12-09 19:12:38 +0800

    + add option: outline tokens + fix: use <code> as target highlighted element instead of <pre>

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -1,25 +1,71 @@
 import { Carousel } from '@mantine/carousel';
 import { Button, Flex, rem, ScrollArea, Space, TextInput, Group, Checkbox } from '@mantine/core';
 import { IconArrowRight, IconArrowLeft } from '@tabler/icons-react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/atom-one-light.min.css';
 
-const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+// const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-function processCode(codeWrapper: HTMLElement) {
-    if (codeWrapper.hasAttribute('data-highlighted')) return;
+function mouseEventLiesIn(e: MouseEvent, ...targetSpans: HTMLElement[]) {
+    return e.target instanceof Node
+        && targetSpans.some((span) => span.contains(e.target as Node));
+}
 
-    console.log(`highlighting ${codeWrapper.outerHTML}`);
-    hljs.highlightElement(codeWrapper);
+function getSelectedElements(selection: Selection, targetSpans: HTMLElement[]) {
+    const selectedElements: HTMLElement[] = [];
+
+    for (let i = 0; i < selection.rangeCount; i++) {
+        const range = selection.getRangeAt(i);
+        const startContainer = range.startContainer;
+        const endContainer = range.endContainer;
+
+        const startSpan = targetSpans.find((span) => span.contains(startContainer));
+        const endSpan = targetSpans.find((span) => span.contains(endContainer));
+        if (startSpan && endSpan) {
+            const _startIndex = targetSpans.indexOf(startSpan);
+            const startIndex = _startIndex < 0 ? 0 : _startIndex;
+            const endIndex = targetSpans.indexOf(endSpan);
+
+            selectedElements.push(...targetSpans.slice(startIndex, endIndex + 1));  // even if endIndex === -1 this works
+        }
+    }
+
+    return selectedElements;
+}
+
+function expandSelectedRanges(selection: Selection, targetSpans: HTMLElement[]) {
+    for (let i = 0; i < selection.rangeCount; i++) {
+        const range = selection.getRangeAt(i);
+        const startContainer = range.startContainer;
+        const endContainer = range.endContainer;
+
+        selection.removeRange(range);
+
+        const startSpan = targetSpans.find((span) => span.contains(startContainer));
+        const endSpan = targetSpans.find((span) => span.contains(endContainer));
+        if (startSpan && endSpan) {
+            range.setStart(startSpan, 0);
+            range.setEnd(endSpan, endSpan.childNodes.length);
+
+            selection.addRange(range);
+        }
+    }
+}
+
+function processCode(code: HTMLElement) {
+    if (code.hasAttribute('data-highlighted')) return;
+
+    console.log(`highlighting ${code.outerHTML}`);
+    hljs.highlightElement(code);
 
     const targetSpans: HTMLElement[] = [];
 
-    codeWrapper.childNodes.forEach((node) => {
+    code.childNodes.forEach((node) => {
         if (!(node instanceof HTMLElement)) {
             const span = document.createElement('span');
             span.textContent = node.textContent;
-            codeWrapper.replaceChild(span, node);
+            code.replaceChild(span, node);
 
             targetSpans.push(span);
         } else {
@@ -61,49 +107,48 @@ function processCode(codeWrapper: HTMLElement) {
     //     });
     // });
 
+    let manualSelectionChange = false;
+
     document.addEventListener('selectionchange', () => {
+        if (manualSelectionChange) {
+            manualSelectionChange = false;
+            return;
+        }
+
         const selection = window.getSelection();
-        if (selection && selection.rangeCount > 0) {
-            const range = selection.getRangeAt(0);
-            const startContainer = range.startContainer;
-            const endContainer = range.endContainer;
-
-            console.log(startContainer, endContainer);
-
-            const startSpan = targetSpans.find((span) => span.contains(startContainer));
-            const endSpan = targetSpans.find((span) => span.contains(endContainer));
-            if (startSpan && endSpan && focused) {
-                const _startIndex = targetSpans.indexOf(startSpan);
-                const startIndex = _startIndex < 0 ? 0 : _startIndex;
-                const endIndex = targetSpans.indexOf(endSpan);
-
-                console.log(`selected range: ${startIndex} - ${endIndex}`);
-
-                targetSpans.forEach((span, i) => {
-                    if (i >= startIndex && i <= endIndex) {
-                        span.classList.add('selected');
-                    } else {
-                        span.classList.remove('selected');
-                    }
-                });
-            } else {
-                targetSpans.forEach((span) => {
+        if (focused && selection && selection.rangeCount > 0) {
+            const selectedElements = new Set(getSelectedElements(selection, targetSpans));
+            targetSpans.forEach((span) => {
+                if (selectedElements.has(span)) {
+                    span.classList.add('selected');
+                } else {
                     span.classList.remove('selected');
-                });
-            }
+                }
+            });
+        } else {
+            targetSpans.forEach((span) => {
+                span.classList.remove('selected');
+            });
         }
     });
 
     let focused = false;
 
     window.addEventListener('mousedown', (e: MouseEvent) => {
-        if (!(e.target instanceof Node
-            && targetSpans.some((span) => span.contains(e.target as Node)))
-        ){
+        if (!mouseEventLiesIn(e, ...targetSpans)) {
             focused = false;
             targetSpans.forEach((span) => {
                 span.classList.remove('selected');
             });
+        }
+    });
+
+    // detail: all the range of the covered spans are selected
+    window.addEventListener('mouseup', () => {
+        const selection = window.getSelection();
+        if (focused && selection && selection.rangeCount > 0) {
+            expandSelectedRanges(selection, targetSpans);
+            manualSelectionChange = true;
         }
     });
 
@@ -128,8 +173,8 @@ export function CodeBlock({ code }: CodeBlockProps) {
     }, [code]);
 
     return (
-        <pre className='target-code-pre' ref={codeRef}>
-            <code>
+        <pre className='target-code-pre'>
+            <code ref={codeRef}>
                 {code}
             </code>
         </pre>
@@ -139,9 +184,11 @@ export function CodeBlock({ code }: CodeBlockProps) {
 export function Feature() {
     const codeCarouselRef = useRef<HTMLDivElement>(null);
 
+    const [optionOutlineTokens, setOptionOutlineTokens] = useState(false);
+
     const samples = [
         'The Python \nLanguage',
-        lorem
+        // lorem
     ];
     const slides = samples.map((sample, i) =>
         <Carousel.Slide key={i}>
@@ -151,8 +198,15 @@ export function Feature() {
         </Carousel.Slide>
     );
 
+    const classList = ['feature-block'];
+    if (optionOutlineTokens) {
+        classList.push('outline-tokens');
+    }
+
+    const className = classList.join(' ');
+
     return (
-        <div className='feature-block' style={{ width: '500px' }}>
+        <div className={className} style={{ width: '500px' }}>
             <TextInput label='Tokens directory' description='Directory that contains tokens of samples' />
             <Flex align='flex-end'>
                 <TextInput label='Results file' description='Labeling results file (.jsonl) of samples' style={{ flexGrow: '1' }} />
@@ -161,7 +215,10 @@ export function Feature() {
             </Flex>
             <Space h='sm'></Space>
             <Group>
-                <Checkbox label='Outline Tokens' />
+                <Checkbox
+                    checked={optionOutlineTokens}
+                    onChange={(event) => setOptionOutlineTokens(event.currentTarget.checked)}
+                    label='Outline Tokens' />
             </Group>
             <Carousel
                 ref={codeCarouselRef}
