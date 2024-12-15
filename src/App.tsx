e892efa4a84a757479105665b7a8ea840c1f8058
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2024-12-16 07:35:58 +0800

    fix: page width will change when there's teacher samples to scroll down

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import './App.css';
 
-import { Center, createTheme, MantineProvider } from '@mantine/core';
+import { Center, createTheme, MantineProvider, ScrollArea } from '@mantine/core';
 import { Feature } from './components/feature';
 
 const theme = createTheme({
@@ -11,10 +11,12 @@ const theme = createTheme({
 
 function App() {
   return (
-      <MantineProvider theme={theme}>
-          <Center>
-            <Feature />
-          </Center>
+    <MantineProvider theme={theme}>
+      <ScrollArea h='100%' p='2em 0'>
+        <Center>
+          <Feature />
+        </Center>
+      </ScrollArea>
     </MantineProvider>
   );
 }
