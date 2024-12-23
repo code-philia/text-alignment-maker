065560864766ea1e0f3e7e45d929865838175dcc
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2024-12-23 08:16:51 +0800

    change navigation style

--- src/components/NumberNavigation.tsx
+++ src/components/NumberNavigation.tsx
@@ -1,3 +1,4 @@
+import { Carousel } from "@mantine/carousel";
 import { Group, Button } from "@mantine/core";
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
@@ -15,81 +16,38 @@ export type NumberNavigationProps = {
 
 export function NumberNavigation({ value, total, onChangeValue, selective, selectedValues, tags }: NumberNavigationProps) {
     const numOfOptions = 6;
-    const [page, setPage] = useState<number>(0);
 
-    useEffect(() => {
-        setPage(Math.floor(value / numOfOptions));
-    }, [value]);
-
-    const totalPages = Math.ceil(total / numOfOptions);
-
-    const startIndex = page * numOfOptions;
-    const endIndex = Math.min(startIndex + numOfOptions, total);
-
-    const isFirstPage = page == 0;
-    const isLastPage = page >= totalPages - 1;
-
-    const isCurrent = (i: number) => value % numOfOptions === i;
-    const isSelected = (i: number) => selectedValues?.includes(i);
-
-    const prevPageValue = Math.max(value - numOfOptions, 0);
-    const nextPageValue = Math.min(value + numOfOptions, total - 1);
+    const isCurrent = (i: number) => value === i;
 
     if (selective && !selectedValues) {
         selectedValues = [value];
     }
 
     const itemsArray = selective ?
-        (
-            new Array(endIndex - startIndex).fill(0).map((_, i) => (
-                <Button
-                    key={i + 1}
-                    className='number-navigation-button'
-                    variant={isSelected(i) ? 'filled' : 'transparent'}
-                    color={isSelected(i) ? 'blue' : 'gray'}
-                    style={isSelected(i) ? {} : {}}
-                    onClick={() => onChangeValue(startIndex + i)}
-                >
-                    {tags ? tags[startIndex + i] : (startIndex + i + 1)}
-                </Button>
-            ))
-        )
+        null
         :
         (
-            new Array(endIndex - startIndex).fill(0).map((_, i) => (
-                <Button
-                    key={i + 1}
-                    className='number-navigation-button'
-                    variant={isCurrent(i) ? 'filled' : 'transparent'}
-                    color={isCurrent(i) ? 'blue' : 'gray'}
-                    onClick={() => onChangeValue(startIndex + i)}
-                >
-                    {tags ? tags[startIndex + i] : (startIndex + i + 1)}
-                </Button>
-            ))
+            <Carousel
+                align='start'
+                p='0 50'
+                slidesToScroll={6}
+                speed={12}
+            >
+                {
+                    new Array(total).fill(0).map((_, i) => (
+                        <Button
+                            key={i + 1}
+                            className='number-navigation-button'
+                            variant={isCurrent(i) ? 'filled' : 'transparent'}
+                            color={isCurrent(i) ? 'blue' : 'gray'}
+                            onClick={() => onChangeValue(i)}
+                        >
+                            {tags ? tags[i] : i + 1}
+                        </Button>
+                    ))
+                }
+            </Carousel>
         );
 
-    return (
-        <Group gap='3'>
-            <Button
-                className='number-navigation-button'
-                variant='transparent'
-                color='gray'
-                disabled={isFirstPage}
-                onClick={() => onChangeValue(prevPageValue)}
-            >
-                <IconArrowLeft />
-            </Button>
-            { itemsArray }
-            <Button
-                className='number-navigation-button'
-                variant='transparent'
-                color='gray'
-                disabled={isLastPage}
-                onClick={() => onChangeValue(nextPageValue)}
-            >
-                <IconArrowRight />
-            </Button>
-        </Group>
-    );
+    return itemsArray;
 }
